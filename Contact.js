3b4a985717e9ed2e129989c17465ad28ecb155fe
Akanksha☺️ <89179275+Enthusiast22@users.noreply.github.com>
2022-02-14 11:30:53 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <h1 align="center">This is the About page</h1>
+        </div>
+    )
+}
+
+export default Contact
